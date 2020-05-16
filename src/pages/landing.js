d1b609d8b630921f9b81ae22dea16e1586f7f572
Inusah Said <saidinusah29@gmail.com>
2020-05-16 15:37:51 +0000

    update

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -3,6 +3,21 @@ import Splash from './splash';
 import Login from './login';
 import AsyncStorage from '@react-native-community/async-storage';
 export default class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timePassed: false,
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setTimePassed();
+    }, 2000);
+  }
+  setTimePassed() {
+    this.setState({timePassed: true});
+  }
+
   render() {
     if (!this.state.timePassed) {
       return <Splash />;
