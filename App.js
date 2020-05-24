fdba7d3eb0f0784ee01ca4a5ba565dd8278c701e
Inusah Said <saidinusah29@gmail.com>
2020-05-24 05:02:56 +0000

    using context for auth

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import Splash from './src/pages/splash';
 import HomePage from './src/pages/homepage';
 import Landing from './src/pages/landing';
 import SignUp from './src/pages/signup';
@@ -13,44 +14,75 @@ import Reminders from './src/pages/reminders';
 import Settings from './src/pages/settings';
 import Support from './src/pages/support';
 import Label from './src/pages/label';
+import {AuthContext} from './src/components/context/context';
+import {View} from 'react-native';
+import {ActivityIndicator} from 'react-native-paper';
 const Drawer = createDrawerNavigator();
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [userToken, setUserToken] = useState(null);
+  const authContext = React.useMemo(() => ({
+    signIn: () => {
+      setUserToken('said');
+      setIsLoading(false);
+    },
+    signOut: () => {
+      setUserToken(null);
+      setIsLoading(false);
+    },
+    signUp: () => {
+      setUserToken('said');
+      setIsLoading(false);
+    },
+  }));
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+  if (isLoading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Splash />
+      </View>
+    );
+  }
+
   return (
-    <NavigationContainer>
-      <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
-        <Drawer.Screen
-          name="Home"
-          component={Landing}
-          options={{headerShown: false}}
-        />
-        <Drawer.Screen
-          name="Sign Up"
-          component={SignUp}
-          options={{headerShown: false}}
-        />
-        <Drawer.Screen
-          name="Login"
-          component={Login}
-          options={{headerShown: false}}
-        />
-        <Drawer.Screen
-          name="Notes"
-          component={HomePage}
-          options={{headerShown: false}}
-        />
-        <Drawer.Screen
-          name="My Notes"
-          component={MyNotes}
-          options={{headerShown: false}}
-        />
-        <Drawer.Screen name="Add Note" component={AddNotes} />
-        <Drawer.Screen name="Reminders" component={Reminders} />
-        <Drawer.Screen name="Settings" component={Settings} />
-        <Drawer.Screen name="Support" component={Support} />
-        <Drawer.Screen name="Label" component={Label} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        <Drawer.Navigator
+          drawerContent={(props) => <DrawerContent {...props} />}>
+          <Drawer.Screen
+            name="Login"
+            component={Login}
+            options={{headerShown: false}}
+          />
+          <Drawer.Screen
+            name="Sign Up"
+            component={SignUp}
+            options={{headerShown: false}}
+          />
+          <Drawer.Screen
+            name="Notes"
+            component={HomePage}
+            options={{headerShown: false}}
+          />
+          <Drawer.Screen
+            name="My Notes"
+            component={MyNotes}
+            options={{headerShown: false}}
+          />
+          <Drawer.Screen name="Add Note" component={AddNotes} />
+          <Drawer.Screen name="Reminders" component={Reminders} />
+          <Drawer.Screen name="Settings" component={Settings} />
+          <Drawer.Screen name="Support" component={Support} />
+          <Drawer.Screen name="Label" component={Label} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 };
 
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,28 +1,6 @@
 import React, {Component} from 'react';
-import Splash from './splash';
 import Login from './login';
-import AsyncStorage from '@react-native-community/async-storage';
-export default class Landing extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      timePassed: false,
-    };
-  }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setTimePassed();
-    }, 2000);
-  }
-  setTimePassed() {
-    this.setState({timePassed: true});
-  }
-
-  render() {
-    if (!this.state.timePassed) {
-      return <Splash />;
-    } else {
-      return <Login navigation={this.props.navigation} />;
-    }
-  }
-}
+const Landing = () => {
+  return <Login />;
+};
+export default Landing;
